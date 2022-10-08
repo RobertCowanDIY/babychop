2ad523debf9cab2047f3ba4f5b5cc30cae740084
zinefer <zinefer@gmail.com>
2022-10-08 12:22:26 -0600

    Simplify code

--- babychop.ino
+++ babychop.ino
@@ -10,15 +10,14 @@ https://www.harborfreight.com/2-in-mini-bench-top-cut-off-saw-62136.html
 //         Pins
 const byte POWER_LED_PIN = 13,
            MOTOR_PWM_PIN = 11,
-           ACTIVE_SW_PIN = 5,
-            SPEED_SW_PIN = 4;
+           ACTIVE_SW_PIN =  5,
+            SPEED_SW_PIN =  4;
 
 //        Config
-const int MIN_POWER = 800,
-          ARM_POWER = 1000,
+const int MIN_POWER =  800, // Match MIN_POWER and MAX_POWER to your ESC config
           MAX_POWER = 2000,
-           LO_POWER = 800,
-           HI_POWER = 1100;
+           LO_POWER = 1000,
+           HI_POWER = 1200;
 
 Servo ESC;
 
@@ -26,42 +25,33 @@ void setup() {
   pinMode(ACTIVE_SW_PIN, INPUT_PULLUP);
   pinMode(SPEED_SW_PIN,  INPUT_PULLUP);
 
-  ESC.attach(11, MIN_POWER, MAX_POWER);
-  ESC.writeMicroseconds(ARM_POWER);
+  ESC.attach(MOTOR_PWM_PIN, MIN_POWER, MAX_POWER);
+  ESC.writeMicroseconds(MIN_POWER);
 }
 
 void loop()
 {
-    if (shouldChop()) {
-         if (digitalRead(SPEED_SW_PIN) == LOW) {
-            lowChop();
+    // ACTIVE_SW_PIN is pulled high when floating
+    // so the saw should be active when the signal 
+    // is low.
+    if (digitalRead(ACTIVE_SW_PIN) == LOW) {
+
+        // SPEED_SW_PIN determines how fast we chop
+        // LOW signal means LO_POWER
+
+        if (digitalRead(SPEED_SW_PIN) == LOW) {
+            ESC.writeMicroseconds(LO_POWER);
+            digitalWrite(POWER_LED_PIN, HIGH);
         } else {
-            highChop();
+            ESC.writeMicroseconds(HI_POWER);
+            digitalWrite(POWER_LED_PIN, HIGH);
         }
+
     } else {
-        noChop();
+        // Disable/turn off the saw
+        ESC.writeMicroseconds(MIN_POWER);
+        digitalWrite(POWER_LED_PIN, LOW);
     }
 
     delay(50);
-}
-
-bool shouldChop() {
-    // ACTIVE_SW_PIN is pulled high when floating so saw should be active
-    // when signal is low
-    return digitalRead(ACTIVE_SW_PIN) == LOW;
-}
-
-void highChop() {
-    ESC.writeMicroseconds(HI_POWER);
-    digitalWrite(POWER_LED_PIN, HIGH);
-}
-
-void lowChop() {
-    ESC.writeMicroseconds(LO_POWER);
-    digitalWrite(POWER_LED_PIN, HIGH);
-}
-
-void noChop() {
-    ESC.writeMicroseconds(MIN_POWER);
-    digitalWrite(POWER_LED_PIN, LOW);
 }
